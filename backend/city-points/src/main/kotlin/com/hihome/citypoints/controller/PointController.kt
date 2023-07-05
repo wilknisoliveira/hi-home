f1f30ea6bee35dd6431ede74e2b32f20f6f297ef
wilknisoliveira <wilknisoliveira@gmail.com>
2023-07-05 08:13:54 -0300

    feat: completing CRUD methods

--- backend/city-points/src/main/kotlin/com/hihome/citypoints/controller/PointController.kt
+++ backend/city-points/src/main/kotlin/com/hihome/citypoints/controller/PointController.kt
@@ -3,7 +3,13 @@ package com.hihome.citypoints.controller
 import com.hihome.citypoints.model.Point
 import com.hihome.citypoints.services.PointService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -13,8 +19,31 @@ class PointController {
     @Autowired
     private lateinit var pointService: PointService
 
+    @PostMapping
+    fun create(@RequestBody point: Point): Point{
+        return pointService.save(point)
+    }
+
     @GetMapping
-    fun findAll(): List<Point>{
+    fun readAll(): List<Point>{
         return pointService.findAll()
+    }
+
+    @GetMapping(value = ["/{id}"])
+    fun readById(@PathVariable(value = "id") id: Long): Point{
+        return pointService.findById(id)
+    }
+
+    @PutMapping
+    fun update(@RequestBody point: Point): Point{
+        return pointService.update(point)
+    }
+
+    @DeleteMapping(value = ["/{id}"])
+    fun update(@PathVariable(value = "id") id: Long): ResponseEntity<*>{
+        pointService.delete(id)
+
+        //I used noContent here to define the 204 status
+        return ResponseEntity.noContent().build<Any>()
     }
 }
--- backend/city-points/src/main/kotlin/com/hihome/citypoints/services/PointService.kt
+++ backend/city-points/src/main/kotlin/com/hihome/citypoints/services/PointService.kt
@@ -1,5 +1,6 @@
 package com.hihome.citypoints.services
 
+import com.hihome.citypoints.exceptions.ResourceNotFoundException
 import com.hihome.citypoints.model.Point
 import com.hihome.citypoints.repository.PointRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,8 +13,38 @@ class PointService {
     private lateinit var pointRepository: PointRepository
     private val logger = Logger.getLogger(PointService::class.java.name)
 
+    fun save(point: Point): Point{
+        logger.info("Creating a new point with the following id: ${point.id}")
+        return pointRepository.save(point)
+    }
+
     fun findAll(): List<Point>{
         logger.info("Getting All")
         return pointRepository.findAll()
     }
+
+    fun findById(id: Long): Point{
+        logger.info("Getting ${id} register")
+        return pointRepository.findById(id)
+            .orElseThrow{ResourceNotFoundException("This point doesn't exist")}
+    }
+
+    fun update(point: Point): Point{
+        logger.info("Updating the following point: ${point.id}")
+        val searchedPoint = pointRepository.findById(point.id)
+            .orElseThrow{ResourceNotFoundException("This point doesn't exist")}
+
+        return pointRepository.save(point)
+    }
+
+    fun delete(id: Long) {
+        logger.info("Deleting a point with the following id: ${id}")
+
+        val searchedPoint = pointRepository.findById(id)
+            .orElseThrow{ResourceNotFoundException("This point doesn't exist")}
+
+        return pointRepository.delete(searchedPoint)
+    }
+
+
 }
