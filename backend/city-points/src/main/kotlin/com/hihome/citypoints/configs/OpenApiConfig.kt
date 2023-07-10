ff1c738dd80a2518288bd7dbd81b1e72710291d4
wilknisoliveira <wilknisoliveira@gmail.com>
2023-07-10 15:18:24 -0300

    chore: Open API Swagger

---
+++ backend/city-points/src/main/kotlin/com/hihome/citypoints/configs/OpenApiConfig.kt
@@ -0,0 +1,26 @@
+package com.hihome.citypoints.configs
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Contact
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.annotations.servers.Server
+
+@OpenAPIDefinition(
+    info = Info(
+        contact = Contact(
+            name = "Wilknis Deyvis",
+            email = "wilknisoliveira@gmail.com"
+        ),
+        description = "OpenApi documentation for City Points project",
+        title = "OpenApi spacification - Wilknis",
+        version = "1.0"
+    ),
+    servers = [
+        Server(
+            description = "Local ENV",
+            url = "http://localhost:8080"
+        )
+    ]
+)
+class OpenApiConfig {
+}
